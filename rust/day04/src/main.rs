208e199b7ddd258c19f9459da8837d1ce74fdc77
cybai <cyb.ai.815@gmail.com>
2022-12-05 12:06:36 +0900

    Implement day 4 part 1

---
+++ rust/day04/src/main.rs
@@ -0,0 +1,48 @@
+use aoc_utils::read_file;
+
+#[derive(Debug)]
+struct Elf {
+    min: u32,
+    max: u32,
+}
+
+struct ElfPair((Elf, Elf));
+
+impl ElfPair {
+    fn fully_overlap(&self) -> bool {
+        let (left, right) = &self.0;
+
+        (left.max <= right.max && left.min >= right.min)
+            || (right.max <= left.max && right.min >= left.min)
+    }
+}
+
+fn main() -> Result<(), ()> {
+    let input = read_file("day04/input")?;
+    let pairs = parse(input);
+
+    println!("Part 1: {}", part1(&pairs));
+
+    Ok(())
+}
+
+fn parse(input: String) -> Vec<ElfPair> {
+    input
+        .lines()
+        .map(|line| {
+            let mut elfs = line.split(",").map(|elf| {
+                let mut digits = elf.split("-").map(|n| n.parse::<u32>().unwrap());
+                let min = digits.next().unwrap();
+                let max = digits.next().unwrap();
+
+                Elf { min, max }
+            });
+
+            ElfPair((elfs.next().unwrap(), elfs.next().unwrap()))
+        })
+        .collect::<Vec<_>>()
+}
+
+fn part1(pairs: &Vec<ElfPair>) -> usize {
+    pairs.iter().filter(|pair| pair.fully_overlap()).count()
+}
